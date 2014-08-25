78c16e21e2cfe8cd93756d365ea6f6484d715a62
megajpc <ruifsa4@gmail.com>
2014-08-25 15:32:05 +0100

    Admin view and model removed from devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :authors
   resources :books
   devise_for :users
-  devise_for :admins
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
